dcaa82eed1ec6f3236360be16d251df1a798cfa0
Emre <emre.ertrk.3455@gmail.com>
2024-07-24 21:20:06 +0300

    added repositories

--- src/main/java/com/hackathon/expense_tracking/repository/PersonRepository.java
+++ src/main/java/com/hackathon/expense_tracking/repository/PersonRepository.java
@@ -1,2 +1,9 @@
-package com.hackathon.expense_tracking.repository;public interface PersonRepository {
+package com.hackathon.expense_tracking.repository;
+
+import com.hackathon.expense_tracking.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person,Long > {
 }
--- src/main/java/com/hackathon/expense_tracking/repository/TransactionRepository.java
+++ src/main/java/com/hackathon/expense_tracking/repository/TransactionRepository.java
@@ -1,2 +1,9 @@
-package com.hackathon.expense_tracking.repository;public interface TransactionRepository {
+package com.hackathon.expense_tracking.repository;
+
+import com.hackathon.expense_tracking.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 }
